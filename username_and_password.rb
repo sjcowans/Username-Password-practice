e95b5053b4890d9fb9d129cba8a42413428a7e79
Sean Cowans <zedesia@yahoo.com>
2023-01-04 12:19:10 -0700

    Add new code

--- username_and_password.rb
+++ username_and_password.rb
@@ -1,28 +1,33 @@
-puts "Welcome! Enter username!"
+puts "Welcome! Enter your name!"
 
-name = gets
-age = 26
-if 
-  name == "Oscar"
-  puts "Hello, Oscar!"
-elsif 
-  name == "Angel" "Grace" "Margaret"
-  puts "Team 7 is the best!"
-else puts "Nice to meet you, #{name}!"
+name = gets.chomp 
+puts "Nice to meet you, #{name}!"   
+
+if
+  name == "Angel" || name == "Margaret" || name == "Grace"
+  puts "Team 7 is the best!" 
+else
+  puts "Let's get started"
 end
 
-if 
-  name = "Sean" && age = 26
+puts "enter age"
+
+age = Integer(gets.chomp)
+  
+puts "Got it!"
+
+
+if name == "Sean" and age == 26
   puts "Hello, Original"
 end
 
-
-puts "Please enter your password"
+puts "Please enter your password."
+puts "Your password must be at least 8 characters and have a # in it."
 loop do
     my_password = gets.chomp
     case
-    when my_password.length < 8
-  puts "Your password is not long enough"
+    when my_password.length <= 8
+  puts "Your password is not long enough. Must be at least 8 characters and contain a #."
     when my_password !~ /[#]/
   puts "Your password must contain a: [#]"
     else
